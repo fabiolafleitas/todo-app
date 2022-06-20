58489633d9b280fbadb40eb3652e728805f6b5e0
Fabiola <fabiola@softwarenatura.com>
2022-06-20 15:01:46 -0400

    Adding enhancers to the store

--- src/exampleAddons/enhancers.js
+++ src/exampleAddons/enhancers.js
@@ -2,6 +2,7 @@ export const sayHiOnDispatch = (createStore) => {
   return (rootReducer, preloadedState, enhancers) => {
     const store = createStore(rootReducer, preloadedState, enhancers)
 
+    // Calls the original dispatch function and then printed 'Hi'
     function newDispatch(action) {
       const result = store.dispatch(action)
       console.log('Hi!')
@@ -16,6 +17,7 @@ export const includeMeaningOfLife = (createStore) => {
   return (rootReducer, preloadedState, enhancers) => {
     const store = createStore(rootReducer, preloadedState, enhancers)
 
+    // Wraps the original getState to modify its behavior
     function newGetState() {
       return {
         ...store.getState(),

c62832c663a129139f1f94921a5874665a82e668
Fabiola <fabiola@softwarenatura.com>
2022-06-20 14:39:09 -0400

    Add todos slice initial state

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -1,7 +1,7 @@
 const initialState = [
-    // { id: 0, text: 'Learn React', completed: true },
-    // { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
-    // { id: 2, text: 'Build something fun!', completed: false, color: 'blue' }
+    { id: 0, text: 'Learn React', completed: true },
+    { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
+    { id: 2, text: 'Build something fun!', completed: false, color: 'blue' }
 ]
 
 const nextTodoId = (todos) => {

92a640b20e9ee78ca8e3293bcd8ff8a607cddb1a
Fabiola <fabiola@softwarenatura.com>
2022-06-20 14:51:58 -0400

    Dispatching some actions

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App'
 
 import './api/server'
+import store from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,3 +12,26 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
+
+console.log('Initial state: ', store.getState())
+
+const unsubscribe = store.subscribe(() =>
+    console.log('State after dispatch: ', store.getState())
+)
+
+store.dispatch({type: 'todos/todoAdded', payload: 'Learn about actions'})
+store.dispatch({type: 'todos/todoAdded', payload: 'Learn about reducers'})
+store.dispatch({type: 'todos/todoAdded', payload: 'Learn about state'})
+
+store.dispatch({type: 'todos/todoToggled', payload: 0})
+store.dispatch({type: 'todos/todoToggled', payload: 1})
+
+store.dispatch({type: 'filters/statusFilterChanged', payload:'Active'})
+store.dispatch({
+    type: 'filters/colorsFilterChanged',
+    payload: { color: 'red', changeType: 'added'}
+})
+
+unsubscribe()
+
+store.dispatch({type: 'todos/todoAdded', payload: 'Try creating a store first'})

472bfc1e1a398eb7f467f12cc711f3be320ee243
Fabiola <fabiola@softwarenatura.com>
2022-06-20 14:43:09 -0400

    Combining slice reducers to create the root reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,60 +1,10 @@
-const initialState = {
-    todos: [
-        { id: 0, text: 'Learn React', completed: true },
-        { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
-        { id: 2, text: 'Build something fun!', completed: false, color: 'blue' }
-    ],
-    filters: {
-        status: 'All',
-        colors: []
-    }
-}
+import todosReducer from './features/todos/todosSlice'
+import filtersReducer from './features/filters/filtersSlice'
 
-const nextTodoId = (todos) => {
-    const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
-    return maxId + 1
-}
-
-export default function appReducer(state = initialState, action) {
-    switch (action.type) {
-        case 'todos/todoAdded': {
-            return {
-                ...state,
-                todos: [
-                    ...state.todos,
-                    {
-                        id: nextTodoId(state.todos),
-                        text: action.payload,
-                        completed: false
-                    }
-                ]
-            }
-        }
-        case 'todos/todoToggled': {
-            return {
-                ...state,
-                todos: state.todos.map(todo => {
-                    if(todo.id !== action.payload){
-                        return todo
-                    }
-                    return {
-                        ...todo,
-                        completed: !todo.completed
-                    }
-                })
-            }
-        }
-        case 'filters/statusFilterChanged': {
-            return {
-                ...state,
-                filters: {
-                    ...state.filters,
-                    status: action.payload
-                }
-            }
-        }
-        default:
-            return state
+export default function rootReducer(state = {}, action) {
+    return {
+        todos: todosReducer(state.todos, action),
+        filters: filtersReducer(state.filters, action)
     }
 }
 

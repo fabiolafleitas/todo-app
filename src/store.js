b36bbeb9aca0ad0dd4400628f1ca03f6c5cacd05
Fabiola <fabiola@softwarenatura.com>
2022-06-22 16:56:54 -0400

    Refactor store setup to use configureStore

--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
-import { applyMiddleware, createStore } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import rootReducer from './reducer'
+import { configureStore } from '@reduxjs/toolkit'
 
-// The thunkMiddleware let us pass functions to dispatch
-const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
+import todosReducer from './features/todos/todosSlice'
+import filtersReducer from './features/filters/filtersSlice'
 
-const store = createStore(rootReducer, composedEnhancer)
+const store = configureStore({
+  reducer: {
+    todos: todosReducer,
+    filters: filtersReducer
+  }
+})
 
 export default store
